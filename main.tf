9649070f06cf6c93d8a8973b0bb96046f01e4c69
PIKKILLI VM CHAKRADAHR <164611883+pikkilichakri234@users.noreply.github.com>
2024-05-14 13:27:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -194,7 +194,7 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   username             = "foo"
-  password             = "Raham#123568i"
+  password             = "om123456"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
